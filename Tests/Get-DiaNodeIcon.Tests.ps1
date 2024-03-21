493e34f77c0b3be7498ead0f9a63cec89002aa22
Jonathan Colon <rebelinux@gmail.com>
2024-03-20 21:34:16 -0400

    Fix ordering

--- Tests/Get-DiaNodeIcon.Tests.ps1
+++ Tests/Get-DiaNodeIcon.Tests.ps1
@@ -5,7 +5,6 @@ BeforeAll {
     }
     $DC = "Server-DC-01v"
     $DCRows = [ordered]@{
-        CPU = "2"
         Memory = "4GB"
     }
     $HTMLOutPut = Get-DiaNodeIcon -Name $DC -IconType "ForestRoot" -Align "Center" -Rows $DCRows -ImagesObj $Images
@@ -14,7 +13,7 @@ BeforeAll {
 
 Describe "Get-DiaNodeIcon" {
     It "Returns a HMLT table" {
-        $HTMLOutPut | Should -BeExactly "<TABLE border='0' cellborder='0' cellspacing='5' cellpadding='0'><TR><TD ALIGN='Center' colspan='1'><img src='RootDomain.png'/></TD></TR><TR><TD align='Center'><B>Server-DC-01v</B></TD></TR><TR><TD align='Center' colspan='1'><FONT POINT-SIZE='14'>CPU: 2</FONT></TD></TR> <TR><TD align='Center' colspan='1'><FONT POINT-SIZE='14'>Memory: 4GB</FONT></TD></TR></TABLE>"
+        $HTMLOutPut | Should -BeExactly "<TABLE border='0' cellborder='0' cellspacing='5' cellpadding='0'><TR><TD ALIGN='Center' colspan='1'><img src='RootDomain.png'/></TD></TR><TR><TD align='Center'><B>Server-DC-01v</B></TD></TR><TR><TD align='Center' colspan='1'><FONT POINT-SIZE='14'>Memory: 4GB</FONT></TD></TR></TABLE>"
     }
     It "Returns a HMLT table with the IMG tag" {
         $HTMLOutPut | Should -Match "<img src='RootDomain.png'/>"
