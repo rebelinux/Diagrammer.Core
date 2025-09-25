16f763f2b4c250fe9b8713c8f984eeb7cc443ce2
Jonathan Colon <rebelinux@gmail.com>
2025-09-24 20:50:56 -0400

    Update .gitignore to include launch.json and modify Example10.ps1 to add -NodeObject parameter in Add-DiaNodeImage

--- Examples/Example10.ps1
+++ Examples/Example10.ps1
@@ -180,7 +180,7 @@ $example10 = & {
             -ImageSizePercent parameter sets the size of the image as a percentage (30% in this case).
         #>
 
-        Add-DiaNodeImage -Name "WAN" -ImagesObj $Images -IconType "Cloud" -IconPath $IconPath -ImageSizePercent 30 -DraftMode:$DraftMode
+        Add-DiaNodeImage -Name "WAN" -ImagesObj $Images -IconType "Cloud" -IconPath $IconPath -ImageSizePercent 30 -NodeObject -DraftMode:$DraftMode
 
         Edge -From WAN -To Router01 @{label = 'Serial0/0'; color = 'black'; fontsize = 18; fontcolor = 'black'; minlen = 2 }
     }
