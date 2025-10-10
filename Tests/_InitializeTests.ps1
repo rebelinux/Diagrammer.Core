f54ac4cadb855a8db93d4651022de956ab85e0ce
Jonathan Colon <rebelinux@gmail.com>
2025-10-09 23:40:04 -0400

    Remove unnecessary blank lines in _InitializeTests.ps1 for improved readability

--- Tests/_InitializeTests.ps1
+++ Tests/_InitializeTests.ps1
@@ -3,9 +3,7 @@ $Script:TestsFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
 $Script:ProjectRoot = Split-Path -Parent $TestsFolder
 $Script:ModuleRoot = Join-Path $ProjectRoot $ModuleName
 $Script:ModuleManifestPath = Join-Path $Script:ModuleRoot ('{0}.psd1' -f $Script:ModuleName)
-
 $Script:PrivateFolder = Join-Path -Path $ProjectRoot -ChildPath ('Src{0}Private' -f [System.IO.Path]::DirectorySeparatorChar)
-
 $Script:GraphvizPath = Join-Path $ProjectRoot 'Graphviz\bin\dot.exe'
 
 if (-not (Get-Module $ModuleName)) {
