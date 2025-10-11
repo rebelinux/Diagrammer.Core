7edec2ad90c55fe87f66d59eebf738d2d4c8bf61
Jonathan Colon <rebelinux@gmail.com>
2025-10-10 22:50:39 -0400

    Remove skip condition from ConvertTo-Pdf-WaterMark test suite for reactivation of tests

--- Tests/ConvertTo-Pdf-WaterMark.Tests.ps1
+++ Tests/ConvertTo-Pdf-WaterMark.Tests.ps1
@@ -3,7 +3,7 @@ BeforeAll {
     . (Join-Path -Path $PrivateFolder -ChildPath 'ConvertTo-Pdf-WaterMark.ps1')
 }
 
-Describe ConvertTo-Pdf-WaterMark -Skip {
+Describe ConvertTo-Pdf-WaterMark {
     BeforeAll {
         # Set the path to the ImageMagick executable (magick.exe)
         $ImageMagickPath = Join-Path $ProjectRoot '\ImageMagick'
